fa429d713db1d054eec4f4d5b83d2b89538ee21b
unknown <mohammed_raad>
2024-05-27 23:30:53 +0300

    chore: add config directory

---
+++ src/config/site.ts
@@ -0,0 +1,17 @@
+export const site = {
+    name: "Blognest",
+    links: [
+        {
+            name: "Blogs",
+            href: "/blogs",
+        },
+        {
+            name: "About",
+            href: "/about",
+        },
+        {
+            name: "contact",
+            href: "/contact",
+        },
+    ],
+}
