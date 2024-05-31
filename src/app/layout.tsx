9e4b460240849401a4fa9aaf2a231884e801326f
unknown <mohammed_raad>
2024-05-31 23:49:07 +0300

    feat: add root metadata

---
+++ src/app/layout.tsx
@@ -0,0 +1,52 @@
+import {site} from "@/config/site"
+import "@/styles/globals.css"
+
+import { GeistSans } from "geist/font/sans"
+import { type Metadata } from "next"
+
+export const metadata: Metadata = {
+    metadataBase: new URL(process.env.NEXT_PUBLIC_APP_URL!),
+    title: {
+        default: `${site.name} - ${site.title}`,
+        template: `%s - ${site.name}`,
+    },
+    description: site.description,
+    keywords: site.keywords,
+    authors: site.authors,
+    creator: site.creator,
+    generator: "Next.js",
+    category: "blog builder",
+    openGraph: {
+        type: "website",
+        locale: "en_US",
+        title: `${site.name} - ${site.title}`,
+        siteName: site.name,
+        description: site.description,
+        url: site.url,
+        images: `${site.url}/og.png`,
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: `${site.name} - ${site.title}`,
+        description: site.description,
+        images: `${site.url}/og.png`,
+        creator: site.creatorLinks.x,
+    },
+    icons: {
+        icon: `${site.url}/logo.png`,
+    },
+}
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="en" className={`${GeistSans.variable}`}>
+            <body>
+                {children}
+            </body>
+        </html>
+    )
+}
--- src/config/site.ts
+++ src/config/site.ts
@@ -1,6 +1,40 @@
 export const site = {
     name: "Blognest",
+    title: "Open-Source Blog builder build with Next.js, Bootstrapped by T3-Stack",
+    description:
+        "Blognest is an open-source blogs builder build with Next.js, Typescript, Tailwind CSS, and Neon. It's bootstrapped by T3-Stack. Build your blog with Ease.",
+    url: "https://blognest-blog.vercel.app",
+    keywords: [
+        "Open-source blog builder app",
+        "next.js",
+        "nextjs app router",
+        "react",
+        "server-action",
+        "zod",
+        "next-auth",
+        "prisma",
+        "typeScript",
+        "tailwindCSS",
+        "blog builder",
+        "e-commerce for books",
+        "neon database",
+        "open-source development",
+        "typeScript app development",
+    ],
     githubLink: "https://github.com/mohmmedraad/blognest",
+    authors: [
+        {
+            name: "mohammed raad",
+            // TODO: add your portfolio link
+        },
+    ],
+    creatorLinks: {
+        github: "https://github.com/mohammedraad/Bookji",
+        githubAccount: "https://github.com/mohammedraad",
+        x: "https://twitter.com/mohammedraad_0",
+        telegram: "https://t.me/mohammedraad_0",
+    },
+    creator: "mohammed raad",
     links: [
         {
             name: "Blogs",
