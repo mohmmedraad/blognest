237f0e6667a00c7a90aa8d22611d3429a43eca10
unknown <mohammed_raad>
2024-06-01 17:14:01 +0300

    feat: change the title metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,15 @@
-import {site} from "@/config/site"
+import { site } from "@/config/site"
+
 import "@/styles/globals.css"
 
-import { GeistSans } from "geist/font/sans"
 import { type Metadata } from "next"
+import { GeistSans } from "geist/font/sans"
 
 export const metadata: Metadata = {
     metadataBase: new URL(process.env.NEXT_PUBLIC_APP_URL!),
     title: {
-        default: `${site.name} - ${site.title}`,
-        template: `%s - ${site.name}`,
+        default: `${site.name} —  ${site.title}`,
+        template: `%s —  ${site.name}`,
     },
     description: site.description,
     keywords: site.keywords,
@@ -19,7 +20,7 @@ export const metadata: Metadata = {
     openGraph: {
         type: "website",
         locale: "en_US",
-        title: `${site.name} - ${site.title}`,
+        title: `${site.name} —  ${site.title}`,
         siteName: site.name,
         description: site.description,
         url: site.url,
@@ -27,7 +28,7 @@ export const metadata: Metadata = {
     },
     twitter: {
         card: "summary_large_image",
-        title: `${site.name} - ${site.title}`,
+        title: `${site.name} —  ${site.title}`,
         description: site.description,
         images: `${site.url}/og.png`,
         creator: site.creatorLinks.x,
@@ -44,9 +45,7 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en" className={`${GeistSans.variable}`}>
-            <body>
-                {children}
-            </body>
+            <body>{children}</body>
         </html>
     )
 }
