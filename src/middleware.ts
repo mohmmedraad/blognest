bc43d34bb0dcd246295a15e7cf50301b51747f9b
unknown <mohammed_raad>
2024-06-02 22:52:50 +0300

    feat: add middleware

---
+++ src/middleware.ts
@@ -0,0 +1,40 @@
+import { type NextRequest } from "next/server"
+import { getToken } from "next-auth/jwt"
+
+import { env } from "./env"
+
+const PUBLIC_ROUTES = ["/", "/blogs", "/login", "/sign-up"]
+const AUTH_ROUTES = ["/dashboard"]
+
+export async function middleware(req: NextRequest) {
+    const token = await getToken({ req, secret: env.NEXTAUTH_SECRET })
+
+    const isLoggedIn = token !== null
+    const route = req.nextUrl.pathname
+
+    const isApiAuthRoute = route.startsWith("/api/auth")
+    const isPublicRoute = PUBLIC_ROUTES.includes(route)
+    const isAuthRoute = AUTH_ROUTES.includes(route)
+
+    if (isApiAuthRoute) {
+        return null
+    }
+
+    if (isAuthRoute && !isLoggedIn) {
+        return Response.redirect(new URL("/login", req.nextUrl))
+    }
+
+    if ((route === "/login" || route === "/sign-up") && isLoggedIn) {
+        return Response.redirect(new URL("/dashboard", req.nextUrl))
+    }
+
+    if (!isPublicRoute && !isLoggedIn) {
+        return Response.redirect(new URL("/login", req.nextUrl))
+    }
+
+    return null
+}
+
+export const config = {
+    matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+}
