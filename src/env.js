8b99b992379ec600a52b967ca0f5864c36489f3b
unknown <mohammed_raad>
2024-06-04 14:23:25 +0300

    chore: remove vercel url

--- src/env.js
+++ src/env.js
@@ -8,7 +8,6 @@ export const env = createEnv({
      */
     server: {
         DATABASE_URL: z.string().url(),
-        VERCEL_URL: z.string().url(),
         NODE_ENV: z
             .enum(["development", "test", "production"])
             .default("development"),
@@ -46,7 +45,6 @@ export const env = createEnv({
      */
     runtimeEnv: {
         DATABASE_URL: process.env.DATABASE_URL,
-        VERCEL_URL: process.env.VERCEL_URL,
         NODE_ENV: process.env.NODE_ENV,
         NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
         NEXTAUTH_URL: process.env.NEXTAUTH_URL,
