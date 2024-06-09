86d7a32995ceb89b70415a0914ccbac76dbec321
unknown <mohammed_raad>
2024-06-09 18:27:21 +0300

    refactor: make nav link more generic

--- src/components/ui/nav-link.tsx
+++ src/components/ui/nav-link.tsx
@@ -12,20 +12,33 @@ interface NavLinkProps
         React.ComponentProps<typeof Link> {
     href: string
     activeClass?: ClassValue
+    matchFullPath?: boolean
     hoverClass?: ClassValue
     asChild?: boolean
 }
 
 const NavLink = React.forwardRef<HTMLAnchorElement, NavLinkProps>(
-    ({ href, className, activeClass, children, hoverClass, ...props }, ref) => {
+    (
+        {
+            href,
+            className,
+            matchFullPath = true,
+            activeClass,
+            children,
+            hoverClass,
+            ...props
+        },
+        ref
+    ) => {
         const pathname = usePathname()
-        const isActive =
-            href === "/" ? pathname === href : pathname.includes(href)
+        const isActive = matchFullPath
+            ? pathname === href
+            : pathname.includes(href)
         if (isActive) className = cn(className, activeClass)
 
         return (
             <Link
-            ref={ref}
+                ref={ref}
                 href={href}
                 className={cn(className, !isActive && hoverClass)}
                 {...props}
