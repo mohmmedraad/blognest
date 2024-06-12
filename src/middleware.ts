ff7049726d993eb0155aa9b3995ef5f9b3469ec1
unknown <mohammed_raad>
2024-06-12 13:13:03 +0300

    chore: remove uploading for the protected routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import { getToken } from "next-auth/jwt"
 import { env } from "./env"
 
 const PUBLIC_ROUTES = ["/", "/blogs", "/login", "/sign-up", "/verify"]
-const AUTH_ROUTES = ["/dashboard", "/api/uploadthing"]
+const AUTH_ROUTES = ["/dashboard"]
 
 export async function middleware(req: NextRequest) {
     const token = await getToken({ req, secret: env.NEXTAUTH_SECRET })
@@ -12,7 +12,7 @@ export async function middleware(req: NextRequest) {
     const isLoggedIn = token !== null
     const route = req.nextUrl.pathname
 
-    const isApiAuthRoute = route.startsWith("/api/auth")
+    const isApiAuthRoute = route.startsWith("/api")
     const isPublicRoute = PUBLIC_ROUTES.some((_route) =>
         _route.startsWith(route)
     )
