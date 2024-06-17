af8543d5518ec0b564287c972535fe26d70eed03
unknown <mohammed_raad>
2024-06-17 14:09:00 +0300

    refactor: update next.config.mjs to include rsc for MillionLint

--- next.config.mjs
+++ next.config.mjs
@@ -11,7 +11,9 @@ const nextConfig = {
 }
 
 const millionConfig = {
-    auto: true, // if you're using RSC: auto: { rsc: true },
+    auto: {
+        rsc: true,
+    },
 }
 
 export default million.next(nextConfig, millionConfig)
