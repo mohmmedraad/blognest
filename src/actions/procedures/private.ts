6706f99be6c5377f7b39ef2085ceebf4b4587611
unknown <mohammed_raad>
2024-06-20 17:48:29 +0300

    chore: Setup ZSA library

---
+++ src/actions/procedures/private.ts
@@ -0,0 +1,16 @@
+"use server"
+
+import { getServerAuthSession } from "@/server/auth"
+import { createServerActionProcedure, ZSAError } from "zsa"
+
+export const privateProcedure = createServerActionProcedure().handler(
+    async () => {
+        const session = await getServerAuthSession()
+
+        if (!session?.user) {
+            throw new ZSAError("NOT_AUTHORIZED")
+        }
+
+        return { user: session.user }
+    }
+)
