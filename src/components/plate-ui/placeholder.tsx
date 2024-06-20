5d17df79f36044a561fd3ba9873bee9519a6c01b
unknown <mohammed_raad>
2024-06-21 00:25:38 +0300

    fix: typescript errors

--- src/components/plate-ui/placeholder.tsx
+++ src/components/plate-ui/placeholder.tsx
@@ -1,50 +1,56 @@
-import React from 'react';
-import { cn } from '@udecode/cn';
+import React, { ReactNode } from "react"
+import { cn } from "@udecode/cn"
 import {
-  createNodeHOC,
-  createNodesHOC,
-  PlaceholderProps,
-  usePlaceholderState,
-} from '@udecode/plate-common';
-import { ELEMENT_H1 } from '@udecode/plate-heading';
-import { ELEMENT_PARAGRAPH } from '@udecode/plate-paragraph';
+    createNodeHOC,
+    createNodesHOC,
+    PlaceholderProps,
+    usePlaceholderState,
+} from "@udecode/plate-common"
+import { ELEMENT_H1 } from "@udecode/plate-heading"
+import { ELEMENT_PARAGRAPH } from "@udecode/plate-paragraph"
 
 export const Placeholder = (props: PlaceholderProps) => {
-  const { children, placeholder, nodeProps } = props;
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const { children, placeholder, nodeProps } = props
 
-  const { enabled } = usePlaceholderState(props);
+    const { enabled } = usePlaceholderState(props)
 
-  return React.Children.map(children, (child) => {
-    return React.cloneElement(child, {
-      className: child.props.className,
-      nodeProps: {
-        ...nodeProps,
-        className: cn(
-          enabled &&
-            'before:absolute before:cursor-text before:opacity-30 before:content-[attr(placeholder)]'
-        ),
-        placeholder,
-      },
-    });
-  });
-};
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return React.Children.map(children, (child) => {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+        return React.cloneElement(child, {
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
+            className: child.props.className,
+            nodeProps: {
+                ...nodeProps,
+                className: cn(
+                    enabled &&
+                        "before:absolute before:cursor-text before:opacity-30 before:content-[attr(placeholder)]"
+                ),
+                placeholder,
+            },
+        })
+    })
+}
 
-export const withPlaceholder = createNodeHOC(Placeholder);
-export const withPlaceholdersPrimitive = createNodesHOC(Placeholder);
+export const withPlaceholder = createNodeHOC(Placeholder)
+export const withPlaceholdersPrimitive = createNodesHOC(Placeholder)
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const withPlaceholders = (components: any) =>
-  withPlaceholdersPrimitive(components, [
-    {
-      key: ELEMENT_PARAGRAPH,
-      placeholder: 'Type a paragraph',
-      hideOnBlur: true,
-      query: {
-        maxLevel: 1,
-      },
-    },
-    {
-      key: ELEMENT_H1,
-      placeholder: 'Untitled',
-      hideOnBlur: false,
-    },
-  ]);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    withPlaceholdersPrimitive(components, [
+        {
+            key: ELEMENT_PARAGRAPH,
+            placeholder: "Type a paragraph",
+            hideOnBlur: true,
+            query: {
+                maxLevel: 1,
+            },
+        },
+        {
+            key: ELEMENT_H1,
+            placeholder: "Untitled",
+            hideOnBlur: false,
+        },
+    ])
