cd0aaed2f03583f01d5921107718429d8654e26b
unknown <mohammed_raad>
2024-06-23 01:30:44 +0300

    feat: make updateArticleContentAction for updating article content

---
+++ src/actions/articles/update-article-content-action.ts
@@ -0,0 +1,35 @@
+"use server"
+
+import { privateProcedure } from "@/actions/procedures/private"
+import { db } from "@/server/db"
+import { ZSAError } from "zsa"
+
+import { updateArticleContentSchema } from "@/lib/validations/articles"
+
+export const updateArticleContentAction = privateProcedure
+    .createServerAction()
+    .input(updateArticleContentSchema)
+    .handler(async ({ ctx, input }) => {
+        const isArticleExist = await db.article.findFirst({
+            where: {
+                id: input.id,
+                site: {
+                    userId: ctx.user.id,
+                },
+            },
+        })
+
+        if (!isArticleExist) {
+            throw new ZSAError("NOT_FOUND")
+        }
+
+        await db.article.update({
+            data: {
+                content: input.content,
+            },
+            where: {
+                id: input.id,
+                userId: ctx.user.id,
+            },
+        })
+    })
