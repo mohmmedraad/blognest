b7a485c46ef770f4d7e3f629e09ed32e2e4974f8
unknown <mohammed_raad>
2024-06-22 23:39:04 +0300

    refactor: remove comments provider

--- src/components/plate-editor.tsx
+++ src/components/plate-editor.tsx
@@ -3,15 +3,12 @@
 import React, { useRef } from "react"
 import { type Prisma } from "@prisma/client"
 import { cn } from "@udecode/cn"
-import { CommentsProvider } from "@udecode/plate-comments"
 import { Plate, type TElement } from "@udecode/plate-common"
 import { DndProvider } from "react-dnd"
 import { HTML5Backend } from "react-dnd-html5-backend"
 
-import { commentsUsers, myUserId } from "@/lib/plate/comments"
 import { MENTIONABLES } from "@/lib/plate/mentionables"
 import { plugins } from "@/lib/plate/plate-plugins"
-import { CommentsPopover } from "@/components/plate-ui/comments-popover"
 import { CursorOverlay } from "@/components/plate-ui/cursor-overlay"
 import { Editor } from "@/components/plate-ui/editor"
 import { FloatingToolbar } from "@/components/plate-ui/floating-toolbar"
@@ -28,41 +25,37 @@ export default function PlateEditor({ content, children }: PlateEditorProps) {
 
     return (
         <DndProvider backend={HTML5Backend}>
-            <CommentsProvider users={commentsUsers} myUserId={myUserId}>
-                <Plate
-                    plugins={plugins}
-                    initialValue={content as unknown as TElement[]}
+            <Plate
+                plugins={plugins}
+                initialValue={content as unknown as TElement[]}
+            >
+                <div
+                    ref={containerRef}
+                    className={cn(
+                        "relative",
+                        // Block selection
+                        "[&_.slate-start-area-left]:!w-[64px] [&_.slate-start-area-right]:!w-[64px] [&_.slate-start-area-top]:!h-4"
+                    )}
                 >
-                    <div
-                        ref={containerRef}
-                        className={cn(
-                            "relative",
-                            // Block selection
-                            "[&_.slate-start-area-left]:!w-[64px] [&_.slate-start-area-right]:!w-[64px] [&_.slate-start-area-top]:!h-4"
-                        )}
-                    >
-                        {children}
+                    {children}
 
-                        <Editor
-                            className="min-h-screen px-[96px] py-16"
-                            autoFocus
-                            focusRing={false}
-                            variant="ghost"
-                            size="md"
-                        />
+                    <Editor
+                        className="min-h-screen px-[96px] py-16"
+                        autoFocus
+                        focusRing={false}
+                        variant="ghost"
+                        size="md"
+                    />
 
-                        <FloatingToolbar>
-                            <FloatingToolbarButtons />
-                        </FloatingToolbar>
+                    <FloatingToolbar>
+                        <FloatingToolbarButtons />
+                    </FloatingToolbar>
 
-                        <MentionCombobox items={MENTIONABLES} />
+                    <MentionCombobox items={MENTIONABLES} />
 
-                        <CommentsPopover />
-
-                        <CursorOverlay containerRef={containerRef} />
-                    </div>
-                </Plate>
-            </CommentsProvider>
+                    <CursorOverlay containerRef={containerRef} />
+                </div>
+            </Plate>
         </DndProvider>
     )
 }
