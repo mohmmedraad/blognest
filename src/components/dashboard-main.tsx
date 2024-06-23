34fbce8cb8b930f4b7810dff3209ecdbd7692541
unknown <mohammed_raad>
2024-06-23 21:44:58 +0300

    refactor: Update DashboardMain component to accept a className prop

--- src/components/dashboard-main.tsx
+++ src/components/dashboard-main.tsx
@@ -1,9 +1,13 @@
 import { type FC, type HTMLAttributes } from "react"
 
+import { cn } from "@/lib/utils"
+
 interface DashboardMainProps extends HTMLAttributes<HTMLDivElement> {}
 
-const DashboardMain: FC<DashboardMainProps> = ({ children }) => {
-    return <main className="w-full p-6 sm:p-8">{children}</main>
+const DashboardMain: FC<DashboardMainProps> = ({ children, className }) => {
+    return (
+        <main className={cn("w-full p-6 sm:p-8", className)}>{children}</main>
+    )
 }
 
 export default DashboardMain
