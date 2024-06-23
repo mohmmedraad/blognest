5113b3bc3e17025992b40dfa19189e7a3cf6fbe6
unknown <mohammed_raad>
2024-06-23 22:56:19 +0300

    refactor: Update AuthorSelect component to use defaultValue instead of value prop

--- src/components/author-select.tsx
+++ src/components/author-select.tsx
@@ -12,30 +12,22 @@ import {
     SelectTrigger,
     SelectValue,
 } from "./ui/select"
-import { Skeleton } from "./ui/skeleton"
 
 interface SiteAuthorProps {
-    value?: string
+    defaultValue?: string
     onValueChange?: (value: string) => void
 }
 
-export function AuthorSelect({ value, onValueChange }: SiteAuthorProps) {
-    const { authors, isFetching } = useAuthorsSelectOptions()
+export function AuthorSelect({ defaultValue, onValueChange }: SiteAuthorProps) {
+    const { authors } = useAuthorsSelectOptions()
 
     return (
-        <Select value={value} onValueChange={onValueChange}>
-            <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder="Select a fruit" />
+        <Select defaultValue={defaultValue} onValueChange={onValueChange}>
+            <SelectTrigger>
+                <SelectValue placeholder="Select an author" />
             </SelectTrigger>
             <SelectContent>
                 <SelectGroup>
-                    {isFetching &&
-                        new Array(5).fill(0).map((_, i) => (
-                            <div key={i} className="flex flex-col gap-2">
-                                <Skeleton className="h-9 w-full" />
-                            </div>
-                        ))}
-
                     {authors.map((author) => (
                         <SelectItem key={author.id} value={author.id}>
                             <div className="flex items-center gap-2">
