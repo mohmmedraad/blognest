11b807232089c5a03d7d0d172ac0849e86f6fc46
unknown <mohammed_raad>
2024-06-23 21:43:01 +0300

    refactor: Add NewArticleErrorPage component for displaying error when creating articles

---
+++ src/app/dashboard/articles/new-article/error.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+import { type FC } from "react"
+import Link from "next/link"
+import { CircleX } from "lucide-react"
+
+import { buttonVariants } from "@/components/ui/button"
+import DashboardMain from "@/components/dashboard-main"
+import RefreshButton from "@/components/refresh-button"
+
+interface NewArticleErrorPageProps {}
+
+const NewArticleErrorPage: FC<NewArticleErrorPageProps> = ({}) => {
+    return (
+        <DashboardMain className="flex items-center justify-center">
+            <div>
+                <div className="mx-auto max-w-max rounded-xl border border-gray-300 p-3.5">
+                    <CircleX className="size-7 text-gray-700" />
+                </div>
+                <p className="mx-auto mt-10 text-center text-4xl font-bold text-gray-900">
+                    Error creating article
+                </p>
+                <p className=" mx-auto mt-4 max-w-[512px] text-center text-xl text-gray-600">
+                    An error occurred while creating the article. Please try
+                    again later.
+                </p>
+                <div className="mx-auto mt-5 flex flex-col justify-center gap-4 sm:flex-row">
+                    <Link
+                        href="/dashboard"
+                        className={buttonVariants({
+                            variant: "outline",
+                        })}
+                    >
+                        Back to dashboard
+                    </Link>
+                    <RefreshButton>Refresh the page</RefreshButton>
+                </div>
+            </div>
+        </DashboardMain>
+    )
+}
+
+export default NewArticleErrorPage
