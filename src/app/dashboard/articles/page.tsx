0fe949cc1124515ff27129b3e05c7b8b1441bba6
unknown <mohammed_raad>
2024-06-25 16:07:36 +0300

    fix: Dynamic server usage

--- src/app/dashboard/articles/page.tsx
+++ src/app/dashboard/articles/page.tsx
@@ -1,5 +1,6 @@
 import { type FC } from "react"
 
+import { getCachedUser } from "@/lib/cached-data"
 import CustomBreadcrumb from "@/components/custom-breadcrumb"
 import DashboardHeading from "@/components/dashboard-heading"
 import DashboardMain from "@/components/dashboard-main"
@@ -8,7 +9,10 @@ import { ArticlesTable } from "./_components/articles-table"
 
 interface ArticlesPageProps {}
 
-const ArticlesPage: FC<ArticlesPageProps> = ({}) => {
+const ArticlesPage: FC<ArticlesPageProps> = async ({}) => {
+    const user = await getCachedUser()
+    if (!user) return null
+
     return (
         <DashboardMain>
             <CustomBreadcrumb pathname={"/dashboard/articles"} />
