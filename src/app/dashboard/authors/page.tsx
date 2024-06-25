94c7cb48195f74d0d017fd992b1f0e8fc236278d
unknown <mohammed_raad>
2024-06-25 15:19:48 +0300

    refactor: remove unused code in AuthorsPage

--- src/app/dashboard/authors/page.tsx
+++ src/app/dashboard/authors/page.tsx
@@ -1,13 +1,10 @@
 import { type FC } from "react"
 import { getServerAuthSession } from "@/server/auth"
-import { db } from "@/server/db"
-import { PackageOpen } from "lucide-react"
 
 import CustomBreadcrumb from "@/components/custom-breadcrumb"
 import DashboardHeading from "@/components/dashboard-heading"
 import DashboardMain from "@/components/dashboard-main"
 
-import AddSiteDialog from "../sites/_components/add-site-dialog"
 import { AuthorsTable } from "./_components/authors-table"
 
 interface AuthorsPageProps {}
@@ -17,40 +14,13 @@ const AuthorsPage: FC<AuthorsPageProps> = async ({}) => {
     if (!session?.user) {
         return
     }
-    const sites = await db.site.findMany({
-        select: {
-            id: true,
-        },
-        where: {
-            userId: session.user.id,
-        },
-    })
-
 
     return (
         <DashboardMain>
             <CustomBreadcrumb pathname={"/dashboard/Authors"} />
             <DashboardHeading>Authors</DashboardHeading>
 
-            {sites.length === 0 ? (
-                <div className="flex size-full flex-col items-center justify-center text-center">
-                    <div className="rounded-xl border border-gray-300 p-3.5">
-                        <PackageOpen className="size-7 text-gray-700" />
-                    </div>
-                    <p className="mt-10 text-4xl font-bold text-gray-900">
-                        NO Sites
-                    </p>
-                    <p className="mt-4 max-w-[512px] text-xl text-gray-600">
-                        Create new site by clicking on the button below, and
-                        start your journey by creating new site.
-                    </p>
-                    <AddSiteDialog size={"xl"} className="mt-8">
-                        Crate site
-                    </AddSiteDialog>
-                </div>
-            ) : (
-                <AuthorsTable />
-            )}
+            <AuthorsTable />
         </DashboardMain>
     )
 }
